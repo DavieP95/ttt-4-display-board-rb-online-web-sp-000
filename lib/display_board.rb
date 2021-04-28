870ac2cacce7841f09aaa063a5814787cea992b3
David Polanco <dayvflight@gmail.com>
2021-04-28 01:12:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+A_tic_tac_toe_board=
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+def display_board
+return A_tic_tac_toe_board
+end
