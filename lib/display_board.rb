190444286ff7899eadb693e698ac5375d126249f
David Polanco <dayvflight@gmail.com>
2021-04-28 01:32:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,6 +13,6 @@ def display_board (array)
   puts "-----------"
   puts " #{array[3]} | #{array[4]} | #{array[5]} "
   puts "-----------"
-  puts " #{array[6]} | #{array[7]}| #{array[8]} "
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 
 end
